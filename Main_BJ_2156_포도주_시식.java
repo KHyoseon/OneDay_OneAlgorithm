75b78defb78ba3e05c24a6b31026da449e1b547a
KHyseon <khs722100@gmail.com>
2022-12-11 16:42:11 +0900

    1211

--- Main_BJ_2156_포도주_시식.java
+++ Main_BJ_2156_포도주_시식.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-public class Main_BJ_2156_포도주_시식_X {
+public class Main_BJ_2156_포도주_시식 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
