ba901aa3f93cf8815452bfcefe6b304f46e84b71
KHyseon <khs722100@gmail.com>
2022-12-11 16:41:35 +0900

    1211

--- Main_BJ_2156_포도주_시식_X.java
+++ Main_BJ_2156_포도주_시식_X.java
@@ -1,35 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main_BJ_2156_포도주_시식_X {
-    static int[][] memo;
-    static int N;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
-        int[] glasses = new int[N];
+        int N = Integer.parseInt(br.readLine());
+        int[] wine = new int[N];
 
         for(int i=0; i<N; i++)
-            glasses[i] = Integer.parseInt(br.readLine());
+            wine[i] = Integer.parseInt(br.readLine());
 
-        // col:0 -> 첫번째로 선택됐을 떼 최댓값, col:1 -> 두번째로 선택됐을 때 최댓값
-        // col == 앞에 선택되었으면 1, 아니면 0
-        memo = new int[N+1][2];
-        System.out.println(selectMax(glasses));
-    }
+        int[][] memo = new int[3][N];
 
-    // 6 10 13 9 8 1
+        memo[0][0] = 0;
+        memo[1][0] = memo[2][0] = wine[0];
 
-    private static int selectMax(int[] glasses) {
-        memo[1][0] = memo[1][1] = glasses[0];
-
-        for(int i=2; i<N+1; i++){
-            memo[i][0] = Math.max(memo[i-1][0]+glasses[i], memo[i][1]);
-            memo[i][1] = Math.max(memo[i - 1][0], memo[i - 1][1]);
+        for(int i=1; i<N; i++){
+            memo[0][i] = max(memo[0][i-1], memo[1][i-1], memo[2][i-1]);
+            memo[1][i] = memo[0][i - 1] + wine[i];
+            memo[2][i] = memo[1][i - 1] + wine[i];
         }
 
-        return Math.max(memo[N][0], memo[N][1]);
+        System.out.print(max(memo[0][N-1], memo[1][N-1], memo[2][N-1]));
+    }
+
+    private static int max(int a, int b, int c) {
+        if(a >= b && a >= c) return a;
+        if(b >= a && b >= c) return b;
+        return c;
     }
 }
