671022fe6a379ee4a1a6b5191475fe46e476356e
Hyoseon Kim <apso123@naver.com>
2023-01-28 23:48:45 +0900

    [Bronze III] Title: 부호, Time: 732 ms, Memory: 128268 KB -BaekjoonHub

---
+++ 백준/Bronze/1247. 부호/부호.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for(int tc=0; tc<3; tc++){
+            int N = Integer.parseInt(br.readLine());
+            BigInteger sum = new BigInteger("0");
+
+            for(int i=0; i<N; i++) {
+                sum = sum.add(new BigInteger(br.readLine()));
+            }
+
+            BigInteger zero = new BigInteger("0");
+            if(sum.compareTo(zero)==0)
+                sb.append("0\n");
+            else if(sum.compareTo(zero)<0)
+                sb.append("-\n");
+            else
+                sb.append("+\n");
+        }
+
+        System.out.println(sb);
+    }
+}
