aec787cc6819a366a2ebba331d3a45807a6bfba2
Hyoseon Kim <apso123@naver.com>
2023-03-05 23:29:51 +0900

    [Silver IV] Title: 보물, Time: 88 ms, Memory: 11732 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[] A, B;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+        A = new int[N];
+        B = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for(int i=0; i<N; i++) {
+            B[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        int sum=0;
+        for(int i=0; i<N; i++) {
+            sum += A[N-1-i] * B[i];
+        }
+
+        System.out.println(sum);
+    }
+}
