ec9b8a7f3e1a473cf8eb8e729ebce22d8e502732
Hyoseon Kim <apso123@naver.com>
2023-03-10 00:23:44 +0900

    Update Main_BJ_14916_거스름돈.java

--- 백준/Main_BJ_14916_거스름돈.java
+++ 백준/Main_BJ_14916_거스름돈.java
@@ -1,16 +1,33 @@
-package code;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main_BJ_14916_거스름돈 {
+public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
-        
-        
+        int change = Integer.parseInt(br.readLine());
+        int five = change / 5;
+        int rest = change - 5*five;
+
+        if(change < 5) {
+            if(change%2 == 0) {
+                System.out.println(change/2);
+            } else {
+                System.out.println("-1");
+            }
+        }
+        else {
+            while (five >= 0) {
+                if (rest % 2 == 0) {
+                    System.out.println(five + rest / 2);
+                    return;
+                }
+                five--;
+                rest += 5;
+            }
+            System.out.println("-1");
+        }
     }
 }
