fc34b8d5bbc2aa31a0352b991c61d26221c1617a
Hyoseon Kim <apso123@naver.com>
2023-03-09 23:49:26 +0900

    Create Main_BJ_14916_거스름돈.java

---
+++ 백준/Main_BJ_14916_거스름돈.java
@@ -0,0 +1,16 @@
+package code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_BJ_14916_거스름돈 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        
+    }
+}
