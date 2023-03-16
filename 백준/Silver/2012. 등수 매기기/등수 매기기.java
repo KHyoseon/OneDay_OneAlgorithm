e135853fc0f9835ffec72b85aaa37acea662c623
Hyoseon Kim <apso123@naver.com>
2023-03-16 21:25:45 +0900

    [Silver III] Title: 등수 매기기, Time: 468 ms, Memory: 52072 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N;
+    static int[] expected;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        N = Integer.parseInt(br.readLine());
+        expected = new int[N+1];
+
+        for (int i = 1; i <= N; i++) {
+            expected[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(expected);
+
+        long sum = 0;
+        for(int sort=1; sort<=N; sort++) {
+            sum += Math.abs(sort - expected[sort]);
+        }
+
+        System.out.println(sum);
+    }
+}
