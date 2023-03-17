52b79753dd41982c8c657cad977113784b24ef72
Hyoseon Kim <apso123@naver.com>
2023-03-17 18:27:46 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.java
+++ 프로그래머스/lv2/87946. 피로도/피로도.java
@@ -1,17 +1,14 @@
 class Solution {
-    int[][] dungeons;
-
-    public int solution(int k, int[][] dg) {
-        dungeons = dg;
-        return comb(k, 0);
+    public int solution(int k, int[][] dungeons) {
+        return comb(k, dungeons, 0);
     }
 
-    private int comb(int k, int flag) {
+    private int comb(int k, int[][] dungeons, int flag) {
         int ret = 0;
         for(int i=0, n=dungeons.length; i<n; i++) {
             if((flag & 1<<i) != 0) continue;
             if(k < dungeons[i][0]) continue;
-            ret = Math.max(ret, comb(k-dungeons[i][1], flag | 1<<i) + 1);
+            ret = Math.max(ret, comb(k-dungeons[i][1], dungeons, flag | 1<<i) + 1);
         }
         return ret;
     }
