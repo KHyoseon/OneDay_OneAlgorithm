1508c477736bd34dec5865a27107e2f56443498d
Hyoseon Kim <apso123@naver.com>
2023-03-23 23:17:11 +0900

    Update Main_BJ_16398_행성_연결.java

--- 백준/Main_BJ_16398_행성_연결.java
+++ 백준/Main_BJ_16398_행성_연결.java
@@ -6,52 +6,57 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main_BJ_16398_행성_연결 {
-    static int N;
-    static ArrayList<PriorityQueue<Edge>> edges;
+    static int N, SUM = 0;
+    static boolean[] selected;
+    static int[][] graph;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
-        edges = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            edges.add(new PriorityQueue<>(Comparator.comparingInt(o -> o.w)));
-        }
+        graph = new int[N][N];
 
         // 그래프 구성
         StringTokenizer st;
-        int w;
         for(int i=0; i<N; i++) {
             st = new StringTokenizer(br.readLine());
             for(int j=0; j<N; j++) {
-                w =  Integer.parseInt(st.nextToken());
-                if(i==j) continue;
-                edges.get(i).add(new Edge(j, w));
+                graph[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // prim
-        System.out.println(prim(0, 0));
+        selected = new boolean[N];
+        selected[0] = true;
+
+        prim(0);
+        System.out.println(SUM);
     }
 
-    private static int prim(int node, int sum) {
-        if(edges.get(node).isEmpty()) return sum;
+    private static void prim(int node) {
+        PriorityQueue<int[]> queue = new PriorityQueue<>((Comparator.comparingInt(o -> o[1])));
 
-        Edge e = edges.get(node).peek();
-        edges.get(node).remove(e);
-        edges.get(e.n).remove(e);
+        boolean done = true;
+        for (boolean s: selected) {
+            if(!s) {
+                done = false;
+                break;
+            }
+        }
 
-        return prim(e.n, sum + e.w);
-    }
+        if(done)    return;
 
-    static class Edge {
-        int n;
-        int w;
+        // 다음 이동 노드 간추리기
+        for(int j=0; j<N; j++) {
+            if(node == j || selected[j]) continue;
+            queue.add(new int[] {j, graph[node][j]});
+        }
 
-        public Edge() {}
-
-        public Edge(int n, int w) {
-            this.n = n;
-            this.w = w;
+        for(int[] next: queue) {
+            if(selected[next[0]]) continue;
+            selected[next[0]] = true;
+            System.out.println(node + " 와 " + next[0] + " 연결, 가중치 "+next[1]);
+            SUM += next[1];
+            prim(next[0]);
         }
     }
 }
