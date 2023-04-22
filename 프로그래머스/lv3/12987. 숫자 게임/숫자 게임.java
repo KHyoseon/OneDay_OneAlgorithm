dba8633fbeafd03aef75bd4364424909cf5e556c
Hyoseon Kim <apso123@naver.com>
2023-04-22 18:41:07 +0900

    [level 3] Title: 숫자 게임, Time: 61.83 ms, Memory: 65.1 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.java
@@ -1,26 +1,15 @@
 import java.util.*;
 class Solution {
-    Deque<Integer> dq = new ArrayDeque<>();
-
     public int solution(int[] A, int[] B) {
         Arrays.sort(A);
         Arrays.sort(B);
 
         int n = A.length;
-        for(int i=n-1; i>=0; i--) {
-            dq.add(B[i]);
-        }
-
         int answer = 0;
-        int target, i=n-1;
-
-        while(i>=0 && !dq.isEmpty()) {
-            target = A[i--];
-            if(dq.peekFirst() > target) {
+        for(int a=n-1, b=a; a>=0 && b>=0; a--) {
+            if(A[a] < B[b]) {
                 answer++;
-                dq.pollFirst();
-            } else if(dq.peekFirst() < target) {
-                dq.pollLast();
+                b--;
             }
         }
 
