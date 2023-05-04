7c25ff41c1f79d6b865a54662a1064eeaa0f6488
Hyoseon Kim <apso123@naver.com>
2023-05-04 17:54:16 +0900

    [Silver III] Title: N과 M (3), Time: 484 ms, Memory: 68500 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int[] list;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        list = new int[M];
+        dfs(0);
+
+        System.out.print(sb);
+    }
+
+    private static void dfs(int cur) {
+        if(cur == M) {
+            for(int l: list)
+                sb.append(l).append(" ");
+            sb.append("\n");
+            return;
+        }
+        for(int d=1; d<=N; d++) {
+            list[cur] = d;
+            dfs(cur+1);
+        }
+    }
+}
