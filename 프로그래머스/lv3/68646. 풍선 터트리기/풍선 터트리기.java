c6f04e11423e8e7533b234b2c0ddfdc4d934a3b4
Hyoseon Kim <apso123@naver.com>
2023-05-08 11:05:44 +0900

    [level 3] Title: 풍선 터트리기, Time: 23.89 ms, Memory: 144 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+class Solution {
+    public int solution(int[] a) {
+        ArrayList<int[]> list = new ArrayList<>();
+        
+        int l = a.length;
+
+        int[] left = new int[l];
+        int[] right = new int[l];
+
+        left[0] = a[0];
+        right[l-1] = a[l-1];
+        
+        for(int i=1; i<l; i++) {
+            left[i] = Math.min(left[i-1], a[i]);
+            right[l-1-i] = Math.min(right[l-i], a[l-i]);
+        }
+
+        if(l==1) return 1;
+        
+        int answer = 2;
+        for(int i=1; i<l-1; i++) {
+            if(left[i] < a[i] && right[i] < a[i]) continue;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
