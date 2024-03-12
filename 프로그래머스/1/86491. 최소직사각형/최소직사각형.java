05522d237dffbc0f065167335d3a21198df0143a
Hyoseon Kim <apso123@naver.com>
2024-03-13 01:09:46 +0900

    [level 1] Title: 최소직사각형, Time: 2.05 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -3,18 +3,10 @@ import java.util.*;
 class Solution {
     public int solution(int[][] sizes) {
         
-        for(int i=0; i<sizes.length; i++) {
-            if(sizes[i][0] < sizes[i][1]) {
-                int temp = sizes[i][1];
-                sizes[i][1] = sizes[i][0];
-                sizes[i][0] = temp;
-            }
-        }
-        
         int max = -1, min = -1;
         for(int[] pair: sizes) {
-            max = Math.max(max, pair[0]);
-            min = Math.max(min, pair[1]);
+            max = Math.max(max, Math.max(pair[0], pair[1]));
+            min = Math.max(min, Math.min(pair[0], pair[1]));
         }
         
         return max * min;
