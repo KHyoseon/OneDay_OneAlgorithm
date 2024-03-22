0c2900ad3444e8620e57f27367c38cba4048e9e3
Hyoseon Kim <apso123@naver.com>
2024-03-22 20:16:56 +0900

    [Gold V] Title: 동전 1, Time: 144 ms, Memory: 18168 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int N, K;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        K = Integer.parseInt(input[1]);
+
+        int[] coins = new int[N+1];
+        int[][] dp = new int[N+1][K+1];
+
+        for(int i=1; i<=N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+            dp[i][0] = 1;
+        }
+
+        for(int i=1; i<=N; i++) {
+            for(int j=1; j<=K; j++) {
+                if (coins[i] > j)
+                    dp[i][j] = dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]];
+            }
+        }
+
+        System.out.println(dp[N][K]);
+    }
+
+}
