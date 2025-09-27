04a204d5f8bb61981642e436838829bf9796787f
Hyoseon Kim <apso123@naver.com>
2025-09-27 18:35:35 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -2,11 +2,6 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
-        int cnt = 0;
-        while(n>0) {
-            if(n%2 != 0) ++cnt;
-            n/=2;
-        }
-        return cnt;
+        return Integer.bitCount(n);
     }
 }
