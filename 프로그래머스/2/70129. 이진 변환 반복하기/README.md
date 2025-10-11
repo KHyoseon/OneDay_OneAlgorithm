e10658ed8c34d2d66ab61e5adf127787fc244cb3
Hyoseon Kim <apso123@naver.com>
2025-10-11 14:15:40 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 22.44 ms, Memory: 95.8 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/README.md
+++ 프로그래머스/2/70129. 이진 변환 반복하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 이진 변환 반복하기 - 70129 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/70129#) 
 
 ### 성능 요약
 
-메모리: 83.5 MB, 시간: 5.35 ms
+메모리: 95.8 MB, 시간: 22.44 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 11일 14:14:13
+2025년 10월 11일 14:15:34
 
 ### 문제 설명
 
--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,33 +2,16 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String s) {
-        int cnt=0, zero=0;
-        int num, one;
-        String str = s;
-        
-        while(!str.equals("1")) {
-            one = oneCount(str);
-            zero += str.length() - one;
-            str = intToStr(one);
-            ++cnt;
+        int[] answer = new int[2];
+        int temp;
+        while( !s.equals("1") ) {
+            answer[1] += s.length();
+            s = s.replaceAll("0", "");
+            temp = s.length();
+            s = Integer.toBinaryString(temp);
+            answer[0]++;
+            answer[1] -= temp;
         }
-        return new int[]{cnt, zero};
-    }
-    
-    public int oneCount(String str) {
-        int one = 0;
-        for(char c: str.toCharArray()) {
-            if(c == '1') ++one;
-        }
-        return one;
-    }
-    
-    public String intToStr(int i) {
-        StringBuilder sb = new StringBuilder();
-        while(i>0) {
-            sb.append(i%2);
-            i /= 2;
-        }
-        return sb.reverse().toString();
+        return answer; 
     }
 }
